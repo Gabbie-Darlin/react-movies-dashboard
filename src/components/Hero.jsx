2d819f792675cd840daffa98afffda4af481824a
Gabbie-Darlin <gabdarlin@gmail.com>
2026-07-10 23:26:43 +0000

    Project initialized and movie components created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+    return (
+        <section className="hero-section">
+            <h1>
+            Discover Amazing TV Shows
+            </h1>
+
+            <p>
+                Search and explore thousands of shows.
+            </p>
+        </section>
+    );
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react';
+import { ReactDOM } from 'react-dom/client.js';
+import "bootstrap/dist/css/bootstrap.min.cs";
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
