26e775ce14b58df903adfcb9ddeb0ad91a194f4f
Gabbie-Darlin <gabdarlin@gmail.com>
2026-07-13 19:17:36 +0000

    TechnologyCard component created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
+import { Link } from "react-router-dom";
+import AppRoutes from "../routes/AppRoutes";
+
 export default function Navbar() {
     return (
         <nav className="navbar navbar-dark bg-dark">
             <div className="container">
                 <span className="navbar-brand">
-                    React Movies Dashboard
+                    Dashboard
                 </span>
+
+                <div>
+                    
+                    <Link className="nav-link" to="/">
+                        Home
+                    </Link>
+
+                    <Link className="nav-link" to="/about">
+                        About
+                    </Link>
+                </div>
             </div>
         </nav>
     );
